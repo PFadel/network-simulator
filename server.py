c68a27ff2268d840efc4ac1464f77573e5acac28
Lucasboden <lucashippertt@id.uff.br>
2017-06-17 13:59:15 -0300

    Conversão para decimal precisa de ajustes

--- server.py
+++ server.py
@@ -1,48 +1,61 @@
 import socket
 import argparse
 import socket_utils
+import _thread
 
+#Parameters
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="aplication desired port", type=int)
 parser.add_argument("neighbors", help="path to neighbors file")
+parser.add_argument("IPs", help="path to IPs file")
 args = parser.parse_args()
 
 port = args.port if args.port is not None else 10000
 
+#Open documents
 with open(args.neighbors, 'r') as neighbors_file:
     neighbors = neighbors_file.read().split('\n')
-max_value = len(neighbors) - 1
+max_value_neig= len(neighbors) - 1
 
-# Create a TCP/IP socket
+with open(args.IPs, 'r') as IPs_file:
+    IPs = IPs_file.read().split('\n')
+max_value_IPs= len(IPs) - 1
+
+#Create a TCP/IP socket
 listening = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ('localhost', port)
+#Bind the socket to the port
+server_address = ('', port)
 print('starting up on {}'.format(server_address))
 listening.bind(server_address)
 
 listening.listen(1)
 
 while True:
+    if max_value_IPs != max_value_neig:
+        print("Quantidade de IPs e de Vizinhos deve ser igual")
+        continue
+    _thread.start_new_thread(socket_utils.listen_socket, tuple([listening,]))
     print('Com qual vizinho se deseja conectar ?')
     for i, n in enumerate(neighbors):
         print('[{}] {}'.format(i, n))
-    print('[{}] Desejo apenas receber conexões'.format(max_value + 1))
-    print('[{}] Terminar execução'.format(max_value + 2))
+    print('[{}] Desejo apenas receber conexões'.format(max_value_neig + 1))
+    print('[{}] Terminar execução'.format(max_value_neig + 2))
     try:
         chosen = int(input())
     except ValueError:
         print('Escolha invalida, deve ser um valor inteiro')
         continue
-    if chosen == max_value + 1:
+    if chosen == max_value_neig + 1:
         socket_utils.listen_socket(listening)
-    elif chosen == max_value + 2:
+    elif chosen == max_value_neig + 2:
         break
-    elif chosen < 0 or chosen > max_value:
+    elif chosen < 0 or chosen > max_value_neig:
         print('Escolha invalida, deve ser um valor entre 0 e {}'.format(
-            max_value))
+            max_value_neig))
         continue
     else:
         address = neighbors[chosen].split(' ')
-        socket_utils.connect_to_neighboor(address)
+        print(socket_utils.toDec(socket_utils.toBin('127', 8) + socket_utils.toBin('0', 8) + socket_utils.toBin('0', 8) + socket_utils.toBin('1', 8)))
+        #socket_utils.connect_to_neighboor(address)      
 print('Execution finished')
--- socket_utils.py
+++ socket_utils.py
@@ -3,30 +3,31 @@ import socket
 
 def listen_socket(socket):
     choice = ''
+    #while True:
+    #print('Aguardando conexao')
+    conn, addr = socket.accept()
+    print("Connection from: " + str(addr))
     while True:
-        print('Aguardando conexao')
-        conn, addr = socket.accept()
-        print("Connection from: " + str(addr))
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            print("Dados recebidos: " + str(data))
-        while True:
-            print('Deseja continuar ouvindo conexoes ? y/n')
-            choice = input().lower()
-            if choice != 'y' and choice != 'n':
-                print('Opcao invalida!')
-                continue
-            break
-        if choice == 'n':
-            break
+        data = conn.recv(1024).decode()
+        if not data:
+           break
+        print("Dados recebidos: " + str(data))
+        break
+        #while True:
+         #   print('Deseja continuar ouvindo conexoes ? y/n')
+          #  choice = input().lower()
+           # if choice != 'y' and choice != 'n':
+            #    print('Opcao invalida!')
+             #   continue
+            #break
+        #if choice == 'n':
+         #   break
 
 
 def connect_to_neighboor(address):
     print('Tentando se conectar com vizinho {} {}'.format(address[0],
                                                           int(address[1])))
-    server_address = (address[0], int(address[1]))
+    server_address = (address[0], int(address[1]))	
     connecting = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connecting.connect(server_address)
     try:
@@ -38,3 +39,61 @@ def connect_to_neighboor(address):
     finally:
         print('closing connection')
         connecting.close()
+		
+#Transforma em binario e completa com 0, para dar o tamanho necessario de bits		
+def toBin(value, length):
+    answer = bin(int(value))[2:]
+    zeros = ''
+    for a in range (0 , 8 - len(answer)):
+        zeros = '0' + zeros
+    return zeros + answer
+
+#Cria o cabeçalho IPV4	
+def createIPV4(orig, dest, dados):
+    version = '0100' #ipv4 = 4
+    ihl = '0101' #5 é o protocolo sem a parte de options
+    typeOfService = '00000000' #requerido pelo professor
+    totalLenght = '?' #16 bits. tamanho do cabeçalho + carga útil
+    identification = '?' # 	16 bits. Identificador utilizado em caso de fragmentação
+    flags = '000' #requerido pelo professor
+    fragmentOffset = '0000000000000' #requerido pelo professor
+    timeToLive = '00000111' #requerido pelo professor
+    protocol = '00000000' #tcp = 6. Professor requeriu 0
+    headerChecksum = '0000000000000000' #requerido pelo professor
+    
+	#transforma cada parte do IP de origem em binario
+    #origens = orig.split('.')
+    sourceIpAddress = ''
+    sourceIpAddress = toBin(orig, 8)
+    #for i in range (0, len(origens)):
+     #   sourceIpAddress = sourceIpAddress + toBin(origens[i], 8)	
+    destinationIpAddress = '?' #professor falou que só vamos utilizar na ultima versão
+    return version + ihl + typeOfService + totalLenght + identification + flags + fragmentOffset + timeToLive + protocol + headerChecksum + sourceIpAddress + destinationIpAddress + dados
+
+def readIPV4(datagram):
+    version = 4  #ipv4 = 4
+    ihl = 5 #5 é o protocolo sem a parte de options
+    typeOfService = 0 #requerido pelo professor
+    totalLenght = '?' #16 bits. tamanho do cabeçalho + carga útil
+    identification = '?' # 	16 bits. Identificador utilizado em caso de fragmentação
+    flags = '000' #requerido pelo professor
+    fragmentOffset = 0 #requerido pelo professor
+    timeToLive = 7 #requerido pelo professor
+    protocol = 0 #tcp = 6. Professor requeriu 0
+    headerChecksum = 0 #requerido pelo professor
+    sourceIpAddress = toDec(datagram[95:127])
+     #   sourceIpAddress = sourceIpAddress + toBin(origens[i], 8)	
+    destinationIpAddress = '?' #professor falou que só vamos utilizar na ultima versão
+    return version + ihl + typeOfService + totalLenght + identification + flags + fragmentOffset + timeToLive + protocol + headerChecksum + sourceIpAddress + destinationIpAddress + dados
+
+#Transforma o IP de binário para decimal
+def toDec(ip):
+    k = 0
+    resp = ''
+    for i in range(1,5):
+        aux = 0
+        for j in range(7,0, -1):
+            aux = aux + (2 ** j) * int(ip[k])
+            k= k+1
+        resp = resp + str(aux) + '.'
+    return resp[0:len(resp)]
