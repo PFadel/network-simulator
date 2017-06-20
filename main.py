a79cbdc21a6a6c7f6131795a303d09cb76d30c47
Lucasboden <lucashippertt@id.uff.br>
2017-06-20 17:30:26 -0300

    main alterada

--- main.py
+++ main.py
@@ -18,10 +18,23 @@ def main(neighbors, IPs, routing, port):
     while True:
         _thread.start_new_thread(socket_utils.listen_socket,
                                  tuple([listening, ]))
-
-        print('Para qual no da rede se deseja enviar uma mensagem ?')
-
-        for i, n in enumerate(routing):
+        print('Digite 1 para enviar mensagem ou 2 para sair')
+        try:
+            option = int(input())
+        except ValueError:
+            print('Escolha invalida, deve ser um valor inteiro')
+            continue
+        if option == 1:
+            print('Digite a mensagem a ser enviada.')
+            mensagem = input()
+            print('Digite o ip de destino.')
+            destination = input()
+            print('Digite a porta.')
+            port = input()
+            socket_utils.route_message(neighbors, IPs, routing, destination)
+        elif option == 2:
+            break		
+        """for i, n in enumerate(routing):
             print('[{}] {}'.format(i, n))
         print('[{}] Terminar execucao'.format(max_value + 1))
         try:
@@ -38,5 +51,5 @@ def main(neighbors, IPs, routing, port):
         else:
             route = routing[chosen].split(' ')[0]
             socket_utils.route_message(neighbors, IPs, routing, route)
-
+"""
     print('Execucao terminada')
